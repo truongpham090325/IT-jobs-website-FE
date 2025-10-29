18c582d9001a1ed924dd25ec094b4ea4484cf740
Phạm Quang Trường <truongpham090325@gmail.com>
2025-10-29 19:19:22 +0700

    1.1. Frontend gửi dữ liệu lên Backend

---
+++ src/app/(pages)/(auth)/user/login/FormLogin.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import JustValidate from "just-validate";
+import { useEffect } from "react";
+
+export default function FormLogin() {
+  useEffect(() => {
+    const validator = new JustValidate("#loginForm");
+
+    validator
+      .addField("#email", [
+        {
+          rule: "required",
+          errorMessage: "Vui lòng nhập email của bạn!",
+        },
+        {
+          rule: "email",
+          errorMessage: "Email không đúng định dạng!",
+        },
+      ])
+      .addField("#password", [
+        {
+          rule: "required",
+          errorMessage: "Vui lòng nhập mật khẩu!",
+        },
+      ])
+      .onSuccess((event: any) => {
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        const dataFinal = {
+          email: email,
+          password: password,
+        };
+
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataFinal),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
+      });
+  }, []);
+
+  return (
+    <>
+      <form id="loginForm" action="" className="grid grid-cols-1 gap-y-[15px]">
+        <div className="">
+          <label
+            htmlFor="email"
+            className="block font-[500] text-[14px] text-black mb-[5px]"
+          >
+            Email *
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+          />
+        </div>
+        <div className="">
+          <label
+            htmlFor="password"
+            className="block font-[500] text-[14px] text-black mb-[5px]"
+          >
+            Mật khẩu *
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+          />
+        </div>
+        <div className="">
+          <button className="bg-[#0088FF] rounded-[4px] w-[100%] h-[48px] px-[20px] font-[700] text-[16px] text-white">
+            Đăng nhập
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
--- src/app/(pages)/(auth)/user/login/page.tsx
+++ src/app/(pages)/(auth)/user/login/page.tsx
@@ -1,9 +1,10 @@
-import { Metadata } from "next"
+import { Metadata } from "next";
+import FormLogin from "./FormLogin";
 
 export const metadata: Metadata = {
   title: "Đăng nhập (Ứng viên)",
   description: "Mô tả trang đăng nhập (Ứng viên)...",
-}
+};
 
 export default function UserLoginPage() {
   return (
@@ -14,38 +15,10 @@ export default function UserLoginPage() {
             <h1 className="font-[700] text-[20px] text-black text-center mb-[20px]">
               Đăng nhập (Ứng viên)
             </h1>
-            <form action="" className="grid grid-cols-1 gap-y-[15px]">
-              <div className="">
-                <label htmlFor="email" className="block font-[500] text-[14px] text-black mb-[5px]">
-                  Email *
-                </label>
-                <input 
-                  type="email" 
-                  name="" 
-                  id="email" 
-                  className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
-                />
-              </div>
-              <div className="">
-                <label htmlFor="password" className="block font-[500] text-[14px] text-black mb-[5px]">
-                  Mật khẩu *
-                </label>
-                <input 
-                  type="password" 
-                  name="" 
-                  id="password" 
-                  className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
-                />
-              </div>
-              <div className="">
-                <button className="bg-[#0088FF] rounded-[4px] w-[100%] h-[48px] px-[20px] font-[700] text-[16px] text-white">
-                  Đăng nhập
-                </button>
-              </div>
-            </form>
+            <FormLogin />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
