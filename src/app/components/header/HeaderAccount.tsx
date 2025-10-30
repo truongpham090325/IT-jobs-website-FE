d1d4bd49bf71c7b4f49b5a7748de66a8edcfd31a
Phạm Quang Trường <truongpham090325@gmail.com>
2025-10-30 14:04:15 +0700

    01. Tính năng Đăng xuất

--- src/app/components/header/HeaderAccount.tsx
+++ src/app/components/header/HeaderAccount.tsx
@@ -1,8 +1,22 @@
 import { useAuth } from "@/hooks/useAuth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const HeaderAccount = () => {
   const { isLogin, infoUser } = useAuth();
+  const router = useRouter();
+
+  const handleLogout = (url: string) => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
+      credentials: "include", // Gửi kèm cookie
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.code == "success") {
+          router.push(url);
+        }
+      });
+  };
 
   return (
     <>
@@ -24,7 +38,10 @@ export const HeaderAccount = () => {
                   Quản lý CV đã gửi
                 </Link>
               </li>
-              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+              <li
+                className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2 cursor-pointer"
+                onClick={() => handleLogout("/user/login")}
+              >
                 <Link href="" className="text-white font-[600] text-[16px]">
                   Đăng xuất
                 </Link>
