5bf9ae16de92aa7e81dfc5729720ba9c82e47cc8
Phạm Quang Trường <truongpham090325@gmail.com>
2025-11-04 09:18:02 +0700

    02. Tạo đường dẫn bảo mật bên Frontend

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+  if (token) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/company-manage/:path*", "/user-manage/:path*"],
+};
