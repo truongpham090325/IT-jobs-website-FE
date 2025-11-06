2fb5c29c9e9b745011fe6a2557efd38a4721a419
Phạm Quang Trường <truongpham090325@gmail.com>
2025-11-06 12:30:06 +0700

    02. Khối Menu trên Header - FE

--- src/app/components/header/HeaderMenu.tsx
+++ src/app/components/header/HeaderMenu.tsx
@@ -16,22 +16,44 @@ export const HeaderMenu = (props: { showMenu: boolean }) => {
           link: "#",
           children: [
             {
-              name: "ReactJS",
-              link: "#",
+              name: "HTML5",
+              link: "/search?language=HTML5",
+            },
+            {
+              name: "CSS3",
+              link: "/search?language=CSS3",
+            },
+            {
+              name: "Javascript",
+              link: "/search?language=Javascript",
             },
             {
               name: "NodeJS",
-              link: "#",
+              link: "/search?language=NodeJS",
+            },
+            {
+              name: "ReactJS",
+              link: "/search?language=ReactJS",
             },
           ],
         },
         {
-          name: "Việc làm IT theo công ty",
-          link: "#",
-        },
-        {
           name: "Việc làm IT theo thành phố",
           link: "#",
+          children: [
+            {
+              name: "Hà Nội",
+              link: "/search?city=Hà Nội",
+            },
+            {
+              name: "Thành phố Hồ Chí Minh",
+              link: "/search?city=Thành phố Hồ Chí Minh",
+            },
+            {
+              name: "Hải Phòng",
+              link: "/search?city=Hải Phòng",
+            },
+          ],
         },
       ],
     },
@@ -41,15 +63,19 @@ export const HeaderMenu = (props: { showMenu: boolean }) => {
       children: [
         {
           name: "FPT Software",
-          link: "#",
+          link: "/search?company=FPT Software",
         },
         {
           name: "Techcombank",
-          link: "#",
+          link: "/search?company=Techcombank",
         },
         {
           name: "MB Bank",
-          link: "#",
+          link: "/search?company=MB Bank",
+        },
+        {
+          name: "Cong ty ABC",
+          link: "/search?company=Cong ty ABC",
         },
       ],
     },
@@ -118,17 +144,17 @@ export const HeaderMenu = (props: { showMenu: boolean }) => {
                       )}
                       {menuSub1.children && (
                         <ul className="lg:absolute relative top-[0px] lg:left-[100%] left-0 lg:w-[280px] w-full bg-[#000065] hidden group-hover/sub-2:block z-[999]">
-                          {menu.children.map((menuSub2, indexSub2) => (
+                          {menuSub1.children.map((menuSub2, indexSub2) => (
                             <li
                               key={indexSub2}
                               className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]"
                             >
-                              <a
+                              <Link
                                 href={menuSub2.link}
                                 className="text-white font-[600] text-[16px]"
                               >
                                 {menuSub2.name}
-                              </a>
+                              </Link>
                             </li>
                           ))}
                         </ul>
